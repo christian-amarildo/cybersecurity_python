847f547a224e475f886da10c6e5a1dab9a4dcbf7
christian-amarildo <127919702+christian-amarildo@users.noreply.github.com>
2023-07-22 21:54:38 -0300

    Update main.py

---
+++ Network Tracking using Wireshark and Google Maps/main.py
@@ -0,0 +1,9 @@
+import dpkt
+import socket
+import pygeoip
+
+gi = retKML(dstip, srcip):
+    dst = gi.record_by_name(dstip)
+    src = gi.record_by_name('x.xxx.xxx.xxx')
+    try:
+        dstlongitude = dst[longitude]
